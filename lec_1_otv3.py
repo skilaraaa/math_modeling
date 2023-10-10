da6e851f7ce3c3e5e85ac01db4bdbd25aa42b7e9
skilaraaa <146107388+skilaraaa@users.noreply.github.com>
2023-10-10 16:47:58 +0000

    fyjytjuuktuf

---
+++ lec_1_otv3.py
@@ -0,0 +1,11 @@
+x = 3
+y = (3/2+16.7*4.32)/(14.5+31/12-x**3.4)
+print(y)
+
+a = (1, 5, "Good", "Bad")
+b = (9, "Blue", "Red", 11)
+print (a[1]+b[3])
+print (a[2]+b[2])
+print (a[0]*b[0])
+print (a[1]**b[3])
+print (a+b)
