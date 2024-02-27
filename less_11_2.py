60516f274a424d822c29f2be9c811576f14e51a7
skilaraaa <146107388+skilaraaa@users.noreply.github.com>
2024-02-27 16:43:02 +0000

    kjhg

---
+++ less_11_2.py
@@ -0,0 +1,13 @@
+import numpy as np 
+from scipy.integrate import odeint
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation 
+frames = 200
+r = np.linspace(0,5, frames)
+
+def move_func(v,t):
+
+
+    return dv_dr
+
+
